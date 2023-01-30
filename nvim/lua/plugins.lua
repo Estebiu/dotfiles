848bcfd25b735b22fb75b6a36352771cc6579951
Estebiu <estebiu@outlook.com>
2023-01-30 22:12:11 +0100

    bootstrapped packer, hope it works pt2

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,27 @@
-return require('packer').startup(function()
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
 -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
 --theme
 	use { "catppuccin/nvim", as = "catppuccin" }
 --Cool effects
@@ -91,4 +112,8 @@ return require('packer').startup(function()
 			'hrsh7th/vim-vsnip',
 		}
 	}
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
+
